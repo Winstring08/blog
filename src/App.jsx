2014f7be110d1ed6b32e30ad196c6d1bcd867163
권승현 <r46t8yh547@gmail.com>
2025-07-21 21:09:11 +0900

    blog 초기 상태

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function AppStart() {
+  let innerData = "데이터"
+  let data = innerData
+  return (
+    <>
+      <div className='App'>
+      <div className="black-nav">
+        <h4>블로그임 {data}</h4>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default AppStart
