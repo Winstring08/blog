dc52d764a1ba4c96bce5e7ebbd24788eb7172112
권승현 <r46t8yh547@gmail.com>
2025-07-22 16:29:52 +0900

    app.css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import './App.css'
+/*eslint-disable*/
 
-function AppStart() {
-  let innerData = "데이터"
-  let data = innerData
+import './App.css'
+import { useState } from 'react';
+
+function App(){
+ 
+  let [title, func] = useState(['강남 우동 맛집', '역삼 우동 맛집', '산곡동 우동 맛집']);
+  let [like, chg] = useState()
   return (
-    <>
-      <div className='App'>
+    <div className="App">
+
       <div className="black-nav">
-        <h4>블로그임 {data}</h4>
+        <div>개발 blog</div>
+        <span>❤️</span>
+        <p></p>
       </div>
+
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
       </div>
-    </>
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+    </div>
   )
 }
 
-export default AppStart
+export default App
